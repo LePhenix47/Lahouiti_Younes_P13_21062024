79967bde5a438399b4fcb8b4563da351bb248006
Younes Lahouiti <loller763@gmail.com>
2024-06-21 11:48:42 +0200

    Fix of the import aliases + addition of the 404 page

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from '@pages/404/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Monde de Dév',
+    component: null,
+  },
+  {
+    path: '/support',
+    title: 'Monde de Dév',
+    component: null,
+  },
+  {
+    path: '**',
+    title: 'Error 404, page not found !',
+    component: NotFoundComponent,
+  },
+];
--- front/src/main.ts
+++ front/src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
+import { environment } from '@environments/environment';
 
 if (environment.production) {
   enableProdMode();
