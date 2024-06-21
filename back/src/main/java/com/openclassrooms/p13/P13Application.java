276d418310edf6c19024b8df6b8060e12eefb6a8
Younes Lahouiti <loller763@gmail.com>
2024-06-21 11:49:00 +0200

    Extraction of all the starter code for the backend

---
+++ back/src/main/java/com/openclassrooms/p13/P13Application.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.p13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P13Application.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/openclassrooms/p13/P13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.p13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
