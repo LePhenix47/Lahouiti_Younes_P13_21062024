2a73fe77d6e205852a823de05ed4204970e69a7b
Younes Lahouiti <loller763@gmail.com>
2024-07-04 11:47:34 +0200

    Addition of some logic to get the HashSet of users who joined the groupchat

--- back/src/main/java/com/openclassrooms/p13/configurations/WebSocketEventListener.java
+++ back/src/main/java/com/openclassrooms/p13/configurations/WebSocketEventListener.java
@@ -1,5 +1,8 @@
 package com.openclassrooms.p13.configurations;
 
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
@@ -20,24 +23,19 @@ public class WebSocketEventListener {
 
     private final SimpMessageSendingOperations messageTemplate;
 
+    // Concurrent set to store usernames of connected users
+    private final Set<String> connectedUsers = ConcurrentHashMap.newKeySet();
+
     @EventListener
     public void onWebSocketConnect(SessionConnectEvent event) {
-        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-
-        String username = (String) headerAccessor.getSessionAttributes().get("username");
-        log.info(headerAccessor.getSessionAttributes().toString());
-
-        if (username == null) {
-            log.info("✖ On WebSocket Connect, user not found for this session for username : " + username);
-        } else {
-            log.info("✔ User : {}" + username + "has connected to chat");
-        }
-
-        var message = new ChatMessage("", username, MessageType.JOIN);
-
-        messageTemplate.convertAndSend("/topic/public", message);
+        log.info("Establishing WebSocket connection...");
     }
 
+    /**
+     * Handles the disconnection of a user from the WebSocket session.
+     *
+     * @param event The SessionDisconnectEvent representing the disconnection event.
+     */
     @EventListener
     public void onWebSocketDisconnect(SessionDisconnectEvent event) {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
@@ -45,14 +43,45 @@ public class WebSocketEventListener {
 
         String username = (String) headerAccessor.getSessionAttributes().get("username");
         if (username == null) {
-            log.debug("✖ On WebSocket Disconnect: " + "User not found for this session for username : " + username);
-        } else {
-            log.info("✔ User : {}" + username + "has disconnected from chat");
+            log.debug("✖ On WebSocket Disconnect: User not found for this session");
+
+            return;
         }
+
+        log.info("✔ User : {} has disconnected from chat", username, connectedUsers);
+        removeConnectedUser(username); // Remove the username from the connected users set
 
         var message = new ChatMessage("", username, MessageType.LEAVE);
 
         messageTemplate.convertAndSend("/topic/public", message);
+    }
+
+    /**
+     * Adds a connected user to the set of connected users.
+     *
+     * @param username the username of the connected user
+     */
+    public void addConnectedUser(String username) {
+        connectedUsers.add(username);
+    }
+
+    /**
+     * Removes a connected user from the set of connected users.
+     *
+     * @param username the username of the user to be removed
+     * @return void
+     */
+    public void removeConnectedUser(String username) {
+        connectedUsers.remove(username);
+    }
+
+    /**
+     * Returns a set of connected user names.
+     *
+     * @return a set of strings representing the connected user names
+     */
+    public Set<String> getConnectedUsers() {
+        return connectedUsers;
     }
 
 }
--- back/src/main/java/com/openclassrooms/p13/controllers/ChatController.java
+++ back/src/main/java/com/openclassrooms/p13/controllers/ChatController.java
@@ -1,5 +1,7 @@
 package com.openclassrooms.p13.controllers;
 
+import java.util.Set;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -7,12 +9,20 @@ import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 import com.openclassrooms.p13.payload.request.ChatMessage;
+import com.openclassrooms.p13.utils.enums.MessageType;
+import com.openclassrooms.p13.configurations.WebSocketEventListener; // Import WebSocketEventListener
 
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
 public class ChatController {
+
+    private final WebSocketEventListener webSocketEventListener;
+
+    public ChatController(WebSocketEventListener webSocketEventListener) {
+        this.webSocketEventListener = webSocketEventListener;
+    }
 
     /**
      * Sends a chat message to all subscribers of the "/topic/public" destination.
@@ -30,7 +40,7 @@ public class ChatController {
 
     /**
      * Adds a user to the chat and sends the chat message to all subscribers of the
-     * "/topic/public" destination.
+     * "/topic/public" destination along with the list of connected users.
      *
      * @param message        the chat message to be sent
      * @param headerAccessor the accessor for the message headers
@@ -41,9 +51,20 @@ public class ChatController {
     public ChatMessage addUser(@Payload ChatMessage message, SimpMessageHeaderAccessor headerAccessor) {
         log.info("New user has arrived : {}", message);
 
-        headerAccessor.getSessionAttributes().put("username", message.sender());
+        // Retrieve username from the message record
+        String username = message.sender();
 
-        return message;
+        // Set username in session attributes
+        headerAccessor.getSessionAttributes().put("username", username);
+
+        // Add the user to connected users set
+        webSocketEventListener.addConnectedUser(username);
+
+        // Retrieve the current list of connected users
+        Set<String> connectedUsers = webSocketEventListener.getConnectedUsers();
+        log.info("Connected users: {}", connectedUsers);
+
+        return new ChatMessage("", username, MessageType.JOIN);
     }
 
 }
