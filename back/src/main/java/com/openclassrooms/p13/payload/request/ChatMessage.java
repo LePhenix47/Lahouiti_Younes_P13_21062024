eed96c60484dc74a309f836f40159bac5457b1e5
Younes Lahouiti <loller763@gmail.com>
2024-07-13 09:44:38 +0200

    Removal of all the Spring Boot backend files to later initialize a Node.js server instead

--- back/src/main/java/com/openclassrooms/p13/payload/request/ChatMessage.java
+++
@@ -1,9 +0,0 @@
-package com.openclassrooms.p13.payload.request;
-
-import com.openclassrooms.p13.utils.enums.MessageType;
-
-public record ChatMessage(
-                String message,
-                String sender,
-                MessageType type) {
-}
