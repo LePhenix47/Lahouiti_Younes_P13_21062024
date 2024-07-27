2ca52e57ed003788bb571a971b6c77ea2ef68068
Younes Lahouiti <loller763@gmail.com>
2024-07-27 15:02:52 +0200

    Fixed the returned value to return an object for the check-username controller

--- back/src/controllers/check-username.controller.cjs
+++ back/src/controllers/check-username.controller.cjs
@@ -5,20 +5,20 @@ exports.hasUsernameBeenTaken = (req, res) => {
     const { userName } = req.body;
 
     if (!req.body || !userName) {
-      res.status(400).send("Bad request, invalid body or username");
+      res.status(400).send({ error: "Bad request, invalid body or username" });
       console.error("Invalid body or username");
 
       return;
     }
 
     if (connectedUsersMap.has(userName)) {
-      res.status(409).send("Username already taken");
+      res.status(409).send({ error: "Username already taken" });
       console.error(userName, "is already taken");
 
       return;
     }
 
-    res.status(200).send("OK !");
+    res.status(200).send({ message: "OK !" });
   } catch (error) {
     res
       .status(500)
