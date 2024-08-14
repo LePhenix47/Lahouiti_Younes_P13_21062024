23c101ffe5b847c1ace9f20d1ad28d523709c02e
Younes Lahouiti <loller763@gmail.com>
2024-08-14 14:35:23 +0200

    Fixed the default I/O devices

--- front/src/app/core/utils/videoconference/videoconference.utils.ts
+++ front/src/app/core/utils/videoconference/videoconference.utils.ts
@@ -8,20 +8,15 @@ export function createDeviceList(
     (device: MediaDeviceInfo) => device.kind === deviceKind
   );
 
-  const combinedDeviceMap = new Map<string, DeviceInfo>();
-  let deviceGroupId: string = '';
+  const commonDeviceLabel: string | null =
+    specificDevicesArray.find((device: MediaDeviceInfo) => {
+      return device.deviceId === 'communications';
+    })?.label || null;
 
+  const arrayOfDevices = [];
   for (const device of specificDevicesArray) {
-    if (device.deviceId === 'default' || device.deviceId === 'communications') {
-      deviceGroupId = device.groupId; // Capture the groupId of the default device
-    }
-
-    if (
-      combinedDeviceMap.has(device.groupId) ||
-      device.deviceId === 'default' ||
-      device.deviceId === 'communications'
-    ) {
-      continue; // Skip further processing for duplicates
+    if (device.deviceId === 'communications' || device.deviceId === 'default') {
+      continue;
     }
 
     // Create a new device object, copying the properties manually
@@ -30,12 +25,12 @@ export function createDeviceList(
       kind: device.kind,
       label: device.label,
       groupId: device.groupId,
-      isDefaultDevice: deviceGroupId === device.groupId,
+      isDefaultDevice: commonDeviceLabel?.includes(device.label) || false,
     };
 
-    combinedDeviceMap.set(device.groupId, newDevice); // Add to the map
+    arrayOfDevices.push(newDevice); // Add to the map
   }
 
   // Convert the map back to an array
-  return Array.from(combinedDeviceMap.values());
+  return arrayOfDevices;
 }
